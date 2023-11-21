458d5c1ff07ea43d0f5e55699371d37c9b1c8b20
TiagoMatarazo <tiagomatarazo@gmail.com>
2023-11-21 14:24:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque = '';
+      switch (this.tipo) {
+        case 'mago':
+          ataque = 'usou magia';
+          break;
+        case 'guerreiro':
+          ataque = 'usou espada';
+          break;
+        case 'monge':
+          ataque = 'usou artes marciais';
+          break;
+        case 'ninja':
+          ataque = 'usou shuriken';
+          break;
+        default:
+          ataque = 'usou um ataque desconhecido';
+      }
+      console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  let meuHeroi = new Heroi('Gandalf', 2019, 'mago');
+  meuHeroi.atacar();
